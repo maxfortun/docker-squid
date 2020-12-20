6e136f4b2c8626aa2cc5308afd002322d0314a05
Max <max@maxf.net>
2020-12-20 12:48:12 -0500

    Remove stale pid file

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -8,6 +8,8 @@ chmod o+rwx /var/cache/squid
 mutex=/var/cache/squid/reload_mutex
 
 [ -d $mutex ] && rmdir $mutex
+[ -f /var/cache/squid/squid.pid ] && rm /var/cache/squid/squid.pid
+
 inotifywait -m -r -e create,modify,delete /etc/squid/ | while read line; do
 	[ -z "$line" ] && continue
 	! mkdir $mutex > /dev/null || ( set +e; sleep 15; squid -k reconfigure; rmdir $mutex; )
