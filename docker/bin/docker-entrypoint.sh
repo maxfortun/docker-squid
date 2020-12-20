31340f2dfc94bbd75ae3b21dd898b4fde80e6686
Max <max@maxf.net>
2020-12-20 10:48:23 -0500

    Adding monitor and auto reload on config change

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -2,12 +2,19 @@
 
 SWD=$(dirname $0)
 
-chmod o+w /dev/stdout
+chmod o+w /dev/stdout 
 chmod o+rwx /var/cache/squid
 
-inotifywait -m -r /etc/squid/ | while read line; do
-	! mkdir /var/cache/squid/reload_needed || ( sleep 15; squid -k reconfigure; rmdir /var/cache/squid/reload_needed; )
+mutex=/var/cache/squid/reload_mutex
+
+[ -d $mutex ] && rmdir $mutex
+inotifywait -m -r -e create,modify,delete /etc/squid/ | while read line; do
+	[ -z "$line" ] && continue
+	! mkdir $mutex > /dev/null || ( set +e; sleep 15; squid -k reconfigure; rmdir $mutex; )
 done &
+watcher=$!
 
 squid -zN
 squid -N
+
+kill $watcher
